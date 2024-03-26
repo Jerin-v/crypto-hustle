3339854dbcd6e70c97b7c9da239b22cb1ad908b8
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-26 16:49:30 -0500

    created component for coin information

---
+++ src/Components/CoinInfo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useEffect, useState } from "react";
+const API_KEY = import.meta.VITE_APP_API_KEY;
+
+const CoinInfo = ({image, name, symbol}) => {
+    const [price, setPrice] = useState(null)
+
+}
+
+export default CoinInfo;
