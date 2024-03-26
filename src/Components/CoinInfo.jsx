f4e25e81f9604a56f40f1f77be515f1f8042683e
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-26 18:54:38 -0500

    fixed broken key

--- src/Components/CoinInfo.jsx
+++ src/Components/CoinInfo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-const API_KEY = import.meta.VITE_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 const CoinInfo = ({image, name, symbol}) => {
     const [price, setPrice] = useState(null)
@@ -9,14 +9,11 @@ const CoinInfo = ({image, name, symbol}) => {
         const getCoinPrice = async () => {
             const response = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=` + API_KEY);
 
-            const json = response.json();
-            setPrice(json)
+            const json = await response.json();
+            setPrice(json);
         }
-
-
         getCoinPrice().catch(console.error);
     }, [symbol])
-
     return (
         <div>
             {price ? (
