d9d50cd61ad269c9e6eb6c6c2a0b284f6b1bb0dd
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-26 13:06:10 -0500

    added function to call API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState(null)
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch("https://min-api.cryptocompare.com/data/all/coinlist?&api_key=" + API_KEY);
+      const json = await response.json()
+      console.log(json)
+      setList(json)
+    }
+
+    fetchAllCoinData().catch(console.error);
+
+  }, []);
+
 
   return (
-    <div>
+    <div className='wholePage'>
+      <h1>My Crypto List</h1>
+        
 
     </div>
   )
