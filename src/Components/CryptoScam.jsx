c6f1d2b2a9044337d336ec839d629c763fdd0b3b
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-27 15:11:41 -0500

    returned list of scam coins

--- src/Components/CryptoScam.jsx
+++ src/Components/CryptoScam.jsx
@@ -7,19 +7,30 @@ const CryptoScam = () => {
     useEffect(() => {
         const getScams = async () => {
             var requestOptions = {
-                method: 'GET',
-                redirect: 'follow'
+                method: "GET",
+                redirect: "follow",
             };
 
             const response = await fetch(
                 "https://api.cryptoscamdb.org/v1/featured",
                 requestOptions
             )
-            const json = response.json()
+            const json = await response.json()
             setScamList(json)
         }
         getScams().catch(console.error)
     }, [])
+
+    return (
+        <div>
+            Here is a list of coins and platforms involved in recent crypto-related scams:
+            <ul className="sideList">
+                {scamList && scamList.result.map((scam) => 
+                    <li key={scam.name}>{scam.name}</li>
+                )}
+            </ul>
+        </div>
+    )
 }
 
 export default CryptoScam;
