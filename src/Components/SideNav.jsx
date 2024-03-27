6d65b58a69bf4d2ce68334c06ce10772cddb8db0
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-27 14:56:58 -0500

    imported crypto scams component

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import CryptoScam from "./CryptoScam";
 
 const SideNav = () => (
     <div className="sideNav">
         <h1>This is my side nav bar</h1>
+        <CryptoScam />
     </div>
 )
 
