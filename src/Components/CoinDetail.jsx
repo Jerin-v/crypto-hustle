ceed6a367231ab77d33d277ca225912d99037c88
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-03 11:59:59 -0500

    added params and use state

--- src/Components/CoinDetail.jsx
+++ src/Components/CoinDetail.jsx
@@ -1,6 +1,11 @@
+import React, {Component, useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 
 const CoinDetail = () => {
+    let params = useParams();
+    const [fullDetails, setFullDetails] = useState(null);
   return (
     <div>
       <h1>Coin Detail</h1>
