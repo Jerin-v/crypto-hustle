7f6803a33c438e1c0a944b260477ee64bdfcb65c
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-03 12:19:25 -0500

    added use effect to fetch coin details

--- src/Components/CoinDetail.jsx
+++ src/Components/CoinDetail.jsx
@@ -6,6 +6,20 @@ const API_KEY = import.meta.env.VITE_APP_API_KEY;
 const CoinDetail = () => {
     let params = useParams();
     const [fullDetails, setFullDetails] = useState(null);
+
+    useEffect(() => {
+        const fetchCoinDetails = async () => {
+            const details = await fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${params.symbol}&tsyms=USD&api_key=${API_KEY}`);
+            const description = await fetch(`https://min-api.cryptocompare.com/data/all/coinlist?fsym=${params.symbol}&api_key=${API_KEY}`);
+            const detailsJson = await details.json();
+            const descriptionJson = await description.json();
+    
+            setFullDetails({"numbers": detailsJson.DISPLAY, "textData": descriptionJson.DATA});
+        }
+        fetchCoinDetails().catch(console.error);
+    
+    }, [params.symbol]);
+
   return (
     <div>
       <h1>Coin Detail</h1>
